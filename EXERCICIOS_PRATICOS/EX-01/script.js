717997add53ea968330f825cf9b4ee897232023f
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-04-23 11:53:36 +0100

    updates

--- EXERCICIOS_PRATICOS/EX-01/script.js
+++ EXERCICIOS_PRATICOS/EX-01/script.js
@@ -2,7 +2,7 @@
 
 const prompt = require('prompt-sync')(); 
 
-let num1= parseInt(prompt("Digite um número: "))
+let num1= parseInt(prompt("Digite um número : "))
 let num2= parseInt(prompt("Digite outro número: "))
 
 let soma= num1+num2
