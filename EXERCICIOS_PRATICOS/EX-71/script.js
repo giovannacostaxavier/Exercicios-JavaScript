5a18bd46cc88a0aad2561dddab0ec245914fd0bc
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-04-28 19:23:05 +0100

    Exercicio 71

--- EXERCICIOS_PRATICOS/EX-71/script.js
+++ EXERCICIOS_PRATICOS/EX-71/script.js
@@ -1,3 +1,13 @@
 //FUNÇÃO QUE RECEBE UM ARRAY E USA FOR...OF PARA CONSTRUIR E RETORNAR UM NOVO ARRAY COM CADA ELEMENTO DUPLICADO
+let construir = (...arr) => {
+    let novoArr = []
 
+    for (const elemento of arr) {
+        novoArr.push(elemento)
+        novoArr.push(elemento)
+    }
 
+    return novoArr
+}
+
+console.log(construir(1,2,3,4))
