f3996825db02711b2b32032aafbe7c5917990da9
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-04-30 20:33:54 +0100

    update

---
+++ EXERCICIOS_PRATICOS/EX-79/script.js
@@ -0,0 +1,13 @@
+/*FUNÇÃO QUE RECEBE A VELOCIDADE DE UM CARRO E USA SWITCH CASE COM INTERVALOS PARA RETORNAR 'NORMAL', 'ATENÇÃO' OU
+'MULTA'*/
+
+let multa = (vel)=>{
+    switch (vel) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
