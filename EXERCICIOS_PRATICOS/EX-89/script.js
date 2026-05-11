6dc82c9892950743cd2620b4c992226ea24108b6
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-05-11 16:32:42 +0100

    Exercicio 89

--- EXERCICIOS_PRATICOS/EX-89/script.js
+++ EXERCICIOS_PRATICOS/EX-89/script.js
@@ -1,2 +1,32 @@
 //FUNÇÃO QUE RECEBE UMA STRING E USA FOR...OF E SWITCH CASE PARA CONTAR QUANTAS LETRAS SÃO 'A', 'E', 'I', 'O' OU 'U'
 
+let vogais =(str)=>{
+    let vg = 0
+    for (const vogais of str) {
+        switch (vogais) {
+            case "a":
+                vg+=1
+                break;
+            case "e":
+                vg+=1
+                break;
+            case "i":
+                vg+=1
+                break;
+            case "o":
+                vg+=1
+                break;
+            case "u":
+                vg+=1
+                break;
+        
+            default:
+                break;
+        }
+    }
+    return vg
+}
+
+let res = vogais("giovanna")
+
+console.log(res);
