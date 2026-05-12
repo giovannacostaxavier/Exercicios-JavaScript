f9effa9d830beb6a5e4ee82d3dcfa7c49a2fcc7b
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-05-12 16:42:48 +0100

    Exercicio 93

--- EXERCICIOS_PRATICOS/EX-93/script.js
+++ EXERCICIOS_PRATICOS/EX-93/script.js
@@ -1,4 +1,19 @@
 /*FUNÇÃO QUE RECEBE UM NÚMERO INTEIRO E USA SWITCH CASE E FOR...OF PARA RETORNAR OS SEUS DIVISORES NUMA FRASE
 DESCRITIVA*/
 
-   
+   let encontrarDivisores = (num) => {
+    let resultado = []
+    let candidatos = Array.from({ length: num }, (_, i) => i + 1)
+    for (let numero of candidatos) {
+        switch (num % numero ) {
+    case 0:
+        resultado.push(numero) 
+        break
+    default:
+        break
+}
+    }
+    console.log(`Os divisores de ${num} são: ${resultado}`)
+}
+
+encontrarDivisores(8)
