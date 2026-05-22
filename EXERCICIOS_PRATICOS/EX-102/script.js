3171f945a525ada9f7c8dc0dfd5e4faf6bf2c8ec
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-05-22 10:13:17 +0100

    study: exercicio usando toUpperCase, trim e length

---
+++ EXERCICIOS_PRATICOS/EX-102/script.js
@@ -0,0 +1,8 @@
+/*RECEBA A STRING " JAVASCRIPT É INCRÍVEL! " (COM ESPAÇOS NAS BORDAS). REMOVA OS ESPAÇOS EXTRAS, CONVERTA TUDO PARA MAIÚSCULO E EXIBA QUANTOS CARACTERES A STRING RESULTANTE POSSUI.*/
+
+let frase = ' Javascript é incrível! '
+let res = frase.toUpperCase().trim()
+console.log(res);
+console.log(res.length);
+
+
