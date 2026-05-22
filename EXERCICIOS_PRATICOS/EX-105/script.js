723a6679bcda878b604b08a80fef1e6e52e663fb
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-05-22 10:57:10 +0100

    study: exercicio usando replaceAll

---
+++ EXERCICIOS_PRATICOS/EX-105/script.js
@@ -0,0 +1,5 @@
+/*RECEBA A FRASE "EU GOSTO DE CAFÉ COM CAFÉ PELA MANHÃ" E SUBSTITUA TODAS AS OCORRÊNCIAS DE "CAFÉ" POR "CHÁ". ATENÇÃO: REPLACE SIMPLES SÓ TROCA A PRIMEIRA — PENSE EM COMO RESOLVER ISSO.*/
+
+let frase = 'Eu gosto de café com café pela manhã'
+
+console.log(frase.replaceAll('café','chá'));
