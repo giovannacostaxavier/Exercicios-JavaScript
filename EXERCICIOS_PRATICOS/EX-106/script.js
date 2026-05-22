c09c82306837183950b905129925d4d4ecb0651b
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-05-22 11:06:20 +0100

    study: exercicio usando slice e substring

---
+++ EXERCICIOS_PRATICOS/EX-106/script.js
@@ -0,0 +1,7 @@
+/*DADO O TEXTO "DESENVOLVIMENTO WEB FULL STACK", EXTRAIA APENAS A PALAVRA "FULL" USANDO SLICE E DEPOIS USANDO SUBSTRING. EXIBA OS RESULTADOS LADO A LADO PARA COMPARAR.*/
+
+let frase = 'Desenvolvimento web full stack'
+
+console.log(`Slice: ${frase.slice(20,24)} | Substring: ${frase.substring(20,24)}`);
+
+
