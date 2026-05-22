4771757bfcf33ce5fae99eb4e660bb6f1cf4baac
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-05-22 09:58:08 +0100

    chore: exercício 100 — concatenação simples e template literal

---
+++ EXERCICIOS_PRATICOS/EX-100/script.js
@@ -0,0 +1,10 @@
+/*DECLARE TRÊS VARIÁVEIS: SEU PRIMEIRO NOME, SEU SOBRENOME E SUA IDADE. MONTE E EXIBA A FRASE "OLÁ, MEU NOME É [NOME] [SOBRENOME] E TENHO [IDADE] ANOS." USANDO CONCATENAÇÃO COM + E DEPOIS REFAÇA USANDO TEMPLATE LITERAL.*/ 
+ let nome = `Giovanna`
+ let sobrenome = `Xavier`
+ let idade = 29
+
+ console.log('Olá, meu nome é '+nome+' '+sobrenome+' e tenho '+idade+ ' anos.');
+
+ console.log(`Olá, meu nome é ${nome} ${sobrenome} e tenho ${idade} anos.`);
+ 
+ 
