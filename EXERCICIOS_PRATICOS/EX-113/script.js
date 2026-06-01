1608df6f3c275190f404ad1a9464ac97612fe786
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-06-01 09:20:03 +0100

    study: exercicio 63 completo

--- EXERCICIOS_PRATICOS/EX-113/script.js
+++ EXERCICIOS_PRATICOS/EX-113/script.js
@@ -1,2 +1,16 @@
 let titulo = document.getElementById('titulo')
 console.log(titulo);
+let paragrafos = document.getElementsByClassName('item')
+console.log(paragrafos);
+let spans = document.getElementsByTagName('span')
+console.log(spans);
+let primeiroP = document.querySelector('.item')
+console.log(primeiroP);
+
+
+let todosP = document.querySelectorAll('.item')
+console.log(todosP);
+let segundoP = document.querySelectorAll('.item')
+console.log(segundoP[1]);
+
+console.log(spans.length);
