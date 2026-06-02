ea278c796bf6738768fc0fe213d09bdc4625ee82
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-06-02 12:16:35 +0100

    study: exercicio novo de metodos de array

---
+++ EXERCICIOS_PRATICOS/EX-119/script.js
@@ -0,0 +1,10 @@
+/*Crie três variáveis com os valores: 15 8 22
+Mostre:
+O maior valor.
+O menor valor.*/
+let n1= 15
+let n2= 8
+let n3=22
+
+console.log(Math.max(n1,n2,n3));
+console.log(Math.min(n1,n2,n3));
