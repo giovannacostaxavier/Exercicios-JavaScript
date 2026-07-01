7a6e99f23f567f873b7d7312555336f707c2c398
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-07-01 19:35:54 +0100

    git commit -m "chore: renomeia pastas de exercicios com nomes descritivos"

---
+++ EXERCICIOS_PRATICOS/EX20-Trim()_ToLowerCase()/script.js
@@ -0,0 +1,6 @@
+/*DADA A STRING "  OLÁ, MUNDO!  ", APLIQUE OS MÉTODOS NECESSÁRIOS PARA OBTER "OLÁ, MUNDO!" (SEM ESPAÇOS E EM MINÚSCULO)*/ 
+
+let frase = `Olá, Mundo!`
+
+console.log(frase.trim());
+console.log(frase.toLowerCase());
---
+++ EXERCICIOS_PRATICOS/EX22-Conversao_Com_Number/script.js
@@ -0,0 +1,5 @@
+//CRIE UMA VARIÁVEL COM O NÚMERO "42" (STRING). CONVERTA PARA NÚMERO, SOME 8 E EXIBA O RESULTADO COM CONSOLE.LOG
+
+let num = '42'
+let novo = Number(num) + 8
+console.log(novo);
