029ab7974c8f2c12336635139dd1dd6ba673a258
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-07-01 19:49:44 +0100

    git commit -m "chore: renomeia pastas de exercicios com nomes descritivos"

---
+++ EXERCICIOS_PRATICOS/EX31-Array_Simples/script.js
@@ -0,0 +1,6 @@
+/*CRIA UM ARRAY COM 5 FRUTAS E IMPRIME O PRIMEIRO E O ÚLTIMO ELEMENTO.*/
+
+let arr = ['Banana','Maça','Uva','Melancia','Melão']
+
+console.log(arr[0],arr[4]);
+
---
+++ EXERCICIOS_PRATICOS/EX33-Add_Com_Push_Unshift/script.js
@@ -0,0 +1,13 @@
+/*USA PUSH PARA ADICIONAR UM ELEMENTO NO FIM E UNSHIFT PARA ADICIONAR OUTRO NO INÍCIO DE UM ARRAY. IMPRIME O ARRAY ANTES E DEPOIS.*/
+
+let arr = ['Banana','Maça','Uva','Melancia','Melão']
+
+console.log(arr);
+
+arr.push('Manga')
+
+console.log(arr);
+
+arr.unshift('Pêra')
+
+console.log(arr);
