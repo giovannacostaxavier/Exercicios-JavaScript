b5bbda2b7deaad878d341fc7a78280865c65d97d
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-07-02 10:22:11 +0000

    chore: renomeia pastas de exercicios com nomes descritivos

--- EXERCICIOS_PRATICOS/EX-120/script.js
+++
@@ -1,9 +0,0 @@
-/*Gere um número aleatório inteiro entre 1 e 10.
-Depois:
-Mostre o número gerado.
-Mostre o mesmo número arredondado para baixo.
-Mostre o mesmo número arredondado para cima.*/
-
-let numeros = Math.floor(Math.random() * 10) + 1
-
-console.log(numeros);
